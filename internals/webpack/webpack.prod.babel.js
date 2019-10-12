32efaeac03b64021d97f69f6293bc91a44898c53
ahmed <ahmed.saab@smaato.com>
2019-10-12 17:19:44 +0200

    feat: logo and browser theme

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -125,16 +125,16 @@ module.exports = require('./webpack.base.babel')({
       short_name: 'React BP',
       description: 'My React Boilerplate-based project!',
       background_color: '#fafafa',
-      theme_color: '#b1624d',
+      theme_color: '#52d0d9',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/logos/512.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/icon-512x512.png'),
+          src: path.resolve('app/images/logos/512.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
--- scripts/run-image.sh
+++ scripts/run-image.sh
@@ -2,4 +2,4 @@
 docker rm -f web-app
 
 # run live (-d for detatched)
-docker run --name web-app -p 3003:3003 easylancer/web-app:latest
+docker run --name web-app -p 3000:3000 easylancer/web-app:latest
